09ec86dc5102cefbb620e071cd218e49ba6749f5
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-06-30 16:09:38 +0100

    First commit

---
+++ classes/order_list.py
@@ -0,0 +1,6 @@
+from classes.order import * 
+
+order1 = Order("Frank", "2020.06.21", "3", "Legend Of Zelda Skyward sword")
+order1 = Order("Sara", "2020.06.22", "2", "Call of Duty Cold War")
+
+orders = [order1]
