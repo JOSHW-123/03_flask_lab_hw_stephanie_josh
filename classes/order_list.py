3a81069509e16f85548f09f504a794662d932c9d
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-06-30 16:52:22 +0100

    MVP Complete

--- classes/order_list.py
+++ classes/order_list.py
@@ -1,6 +1,6 @@
 from classes.order import * 
 
 order1 = Order("Frank", "2020.06.21", "3", "Legend Of Zelda Skyward sword")
-order1 = Order("Sara", "2020.06.22", "2", "Call of Duty Cold War")
+order2 = Order("Sara", "2020.06.22", "2", "Call of Duty Cold War")
 
-orders = [order1]
+orders = [order1, order2]
