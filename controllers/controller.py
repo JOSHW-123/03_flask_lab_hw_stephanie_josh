a62512a62295e0f8a15fac39bf07a4ad60757321
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-06-30 17:24:54 +0100

    Extension started but not finished

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,7 @@ from classes.order_list import orders
 @app.route('/orders')
 def index():
     return render_template('index.html', title='Home', orders=orders)
+
+@app.route('/orders/<index>')
+def index():
+    return 
